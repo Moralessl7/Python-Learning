9b6f86629b7908b4266eeb9a7cae677d9d539a0b
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 19:37:42 -0500

    Add files via upload

---
+++ HelloPy.py
@@ -0,0 +1,13 @@
+name= "Santiago"
+age= 18
+country= "Colombia"
+height= 1.67
+favorite_game= "The Walking Dead"
+print(f"My name is", name, "and I am", age, "years old. I am from", country, "and my height is", height, "meters. My favorite game is", favorite_game + ".")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+country = input("Where are you from? ")
+height = input("What is your height in meters? ")
+favorite_game = input("What is your favorite game? ")
+print("My name is", name, "and I am", age, "years old. I am from", country, "and my height is", height, "meters. My favorite game is", favorite_game + ".")
