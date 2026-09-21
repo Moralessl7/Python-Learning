222e34c5c323905e59020fae2ca2607d5c0e1c40
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 19:39:14 -0500

    Add files via upload

---
+++ lesson02.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter the first number:  "))
+number2 =int(input("enter the second number:  "))
+
+sum_result = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print()
+print("results")
+print("Addition:", sum_result)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
