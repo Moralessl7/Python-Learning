8f688bdc97baaf0d1626166124336aea6424ce3e
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 20:08:28 -0500

    Add files via upload

---
+++ lesson05.py
@@ -0,0 +1,11 @@
+is_student = True
+has_laptop = True
+
+print(is_student)
+print(has_laptop)
+
+if is_student and has_laptop:
+    print("Ready for programming!.")
+else:
+    print("Need more equipment.")
+    
