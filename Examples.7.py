5594582503946eaea81bf4283bdbf6652bc9756e
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 20:14:12 -0500

    Add files via upload

---
+++ Examples.7.py
@@ -0,0 +1,6 @@
+print("=== The infinite addition ===")
+number=int(input("Enter the number you want to add infinitely, DON´T ENTER JUST ZERO:  "))
+number2=int(input("Enter the number you want to add to the first number, DON´T ENTER JUST ZERO:  "))
+while True:
+    print(number + number2)
+    
