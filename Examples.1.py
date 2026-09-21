961b305ac868462b7772b4340d8517ab2e4041f3
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 20:10:43 -0500

    Add files via upload

---
+++ Examples.1.py
@@ -0,0 +1,9 @@
+
+number1 = float(input("Enter the number:  "))
+
+if number1 >= 0.00000000000000000000000000000001:
+    print("The number is positive.")
+elif number1 == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
