6d7ce544c1ecbeaf78cd3d4affa99923efb9e4c2
Moralessl7 <eriksantiagomorales16@gmail.com>
2026-09-20 20:11:59 -0500

    Add files via upload

---
+++ Examples.3.py
@@ -0,0 +1,16 @@
+print("WELCOME TO THE CALCULATOR 3000")
+
+number1= float(input("Enter the first number:   "))
+number2= float(input("Enter the second number:   "))
+number3= float(input("Enter the third number:   "))
+
+addition = number1 + number2 + number3
+subtraction = number1 - number2 - number3
+multiplication = number1 * number2 * number3
+division = number1 / number2 / number3
+
+print("The results of the operations are:  ")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
